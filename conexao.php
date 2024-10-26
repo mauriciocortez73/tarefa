5ffa7014a1f59f2c793c3629e8ff19462a3b47cb
Maurício Cortez <131985501+mauriciocortez73@users.noreply.github.com>
2024-10-26 12:38:24 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $user = 'user';
 $password = 'toor';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo = new PDO("mariadb:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage()); 
